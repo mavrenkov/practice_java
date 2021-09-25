ee8c5fc6fed3f394423f263c93357de60d912b0b
Roman Mavrenkov <mawro6666@gmail.com>
2021-09-24 20:05:50 -0500

    Task day_01 added (#1)

---
+++ src/main/java/day_01/Sequences.java
@@ -0,0 +1,44 @@
+package day_01;
+ //uncomment to execute
+public class Sequences {
+
+    public static void main(String[] args) {
+
+        /*
+        if you want to print double quote -> " - Java will treat it as a special character
+        to avoid it you must add \ beforehand
+        Example:
+         */
+
+     //   System.out.println("\"Java\"");
+
+         /*
+        Also you can add tab or new line using:
+          \t - tab
+          */
+
+     //   System.out.println("\tI am learning\t\"Java\"");
+
+        // \n - new line
+
+     //   System.out.println("\n\tI am learning\n\t\"Java\"");
+
+        // you can even print diamond :)
+
+     //   System.out.println("    ^\n   / \\   \n  /   \\  \n /     \\ \n/_______\\\n\\       /\n \\     /\n  \\   /\n   \\ /\n    v ");
+
+        // or diamond with stars *_*
+
+     //   System.out.println("    ^\n   / \\   \n  / * \\  \n / * * \\ \n/ * * * \\\n_________\n\\ * * * /\n \\ * * /\n  \\ * /\n   \\ /\n    v");
+
+
+
+        //PS: to declare float or long - you need to specify it with f or L
+
+        long l = 999999999999L;
+        float f = 2.5f;
+
+    }
+
+
+}
---
+++ src/main/java/day_01/Warm_up_task_01.java
@@ -0,0 +1,40 @@
+package day_01;
+/*
+        1. create a class named EmployeeInfo
+                    declare the following variables:
+                             firstName
+                             lastName
+                             gender
+                             age
+                             companyName
+                             jobTitle
+                             isFullTime
+                             isMarried
+                             salary
+           write a program that can display the full info of the employee
+                    ex:
+                             firstName = Liubov
+                             lastName = Panchenko
+                             gender = Female
+                             age = 18
+                             companyName = CapitalOne
+                             jobTitle = SDET
+                             isFullTime = true
+                             isMarried = false
+                             salary = 120000.50
+             output:
+                        Employee' full name is: Liubov Panchenko
+                        Liubov Panchenko' gender is: Female
+                        Liubov Panchenko' age is: 18 years old
+                        Liubov Panchenko works at: CapitalOne
+                        Liubov Panchenko' Job title is: CEO
+                        Liubov Panchenko' salary is 120000.5 $
+                        Liubov Panchenko is full time employee: true
+                        Liubov Panchenko is married: false
+
+            PS: Use only on System.out
+ */
+public class Warm_up_task_01 {
+
+}
+
