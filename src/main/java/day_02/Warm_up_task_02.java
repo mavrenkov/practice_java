1a3878968221f19973077501d9d0c5dabb74b981
mavrenkov <mawro@bk.ru>
2021-09-28 08:07:22 -0500

    Task 02 added

---
+++ src/main/java/day_02/Warm_up_task_02.java
@@ -0,0 +1,36 @@
+package day_02;
+
+public class Warm_up_task_02 {
+
+
+    public static void main(String[] args) {
+
+        //1. using String methods, write the method that will capitalize string?
+        //e.g. capitalize(java) = Java
+
+        //2. Write a java method that will tOGGLE string
+        //e.g. toggle(Java) = jAVA
+
+        //3. Question: How many objects will be created in the following code?
+        String s1="I am a String";
+        String s2="I am a String";
+
+        //4. Question: what will be the output:
+        //System.out.println(s1==s2);   true or false?
+        //System.out.println(s1== new String("I am a String")); true or false?
+        //System.out.println(s1.equals(new String("I am a String"))); true or false?
+
+    }
+
+
+    public static String capitalize (String str){
+
+        return str;
+    }
+
+    public static String toggle (String str){
+
+        return str;
+    }
+
+}
