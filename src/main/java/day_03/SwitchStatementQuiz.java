cf710f0d75438e854e052a3a9784fc95b3bfc23a
mavrenkov <mawro@bk.ru>
2021-10-03 13:06:13 -0500

    started switch quiz

---
+++ src/main/java/day_03/SwitchStatementQuiz.java
@@ -0,0 +1,65 @@
+package day_03;
+
+public class SwitchStatementQuiz {
+    //9 questions about SWITCH statement, good luck!
+    public static void main(String[] args) {
+        //1
+        /*
+        long z = 10;
+        switch (z){
+            case 10:
+                System.out.println("Monday");
+            case 11:
+                System.out.println("Tuesday ");
+            case 12:
+                System.out.println("Wednesday");
+            default:
+                System.out.println("Friday");
+
+        }
+         */
+        /*
+        What's the result?
+        1. Monday
+        2. Compile error
+        3. Monday
+           Tuesday
+           Wednesday
+           Friday
+        4. Monday
+           Tuesday
+           Wednesday
+         */
+        //2
+        /*
+        double z =10;
+        switch (z) {
+            case 10:
+                System.out.println("Monday");
+            case 11:
+                System.out.println("Tuesday");
+            case 12:
+                System.out.println("Wednesday");
+            default:
+                System.out.println("Friday");
+        }
+       */
+        /*
+        What's the output?
+        1. Monday
+           Tuesday
+           Wednesday
+           Friday
+        2. Monday
+        3. Compile error
+        4. Monday
+           Tuesday
+           Wednesday
+         */
+        //3
+
+
+
+    }
+
+}
