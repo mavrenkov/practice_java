96abf9f531bf9131fc1180303ead62c2414f7144
mavrenkov <mawro@bk.ru>
2021-10-04 20:36:46 -0500

    swich quiz

--- src/main/java/day_03/SwitchStatementQuiz.java
+++ src/main/java/day_03/SwitchStatementQuiz.java
@@ -1,7 +1,11 @@
 package day_03;
 
+import com.sun.org.apache.xalan.internal.xsltc.cmdline.Compile;
+
+import java.util.zip.Deflater;
+
 public class SwitchStatementQuiz {
-    //9 questions about SWITCH statement, good luck!
+    //4 questions about SWITCH statement, good luck!
     public static void main(String[] args) {
         //1
         /*
@@ -57,6 +61,49 @@ public class SwitchStatementQuiz {
            Wednesday
          */
         //3
+        /*
+        float z =10;
+        switch (z){
+            case 10:
+                System.out.println("Monday");
+            case 11:
+                System.out.println("Tuesday");
+            case 12:
+                System.out.println("Wednesday");
+            default:
+                System.out.println("Friday");
+        }
+        */
+        /*
+        What's the output?
+        1. Monday
+                Tuesday
+        Wednesday
+                Friday
+        2. Monday
+        3. Compile error
+        4. Monday
+                Tuesday
+        Wednesday
+                */
+        //4
+        /*
+        boolean option = true;                      //line 1
+        switch (option){                            //line 2
+            case true:                              //line 3
+                System.out.println("True ");        //line 4
+                break;                              //line 5
+            default:                                //line 6
+                System.out.println("False ");       //line 7
+        }
+           System.out.println("Complete");          //line 8
+         */
+        /*
+        Which modification enables the code fragment to print: True Complete
+        1. replace line 1 with String option=true; replace line 3 with the case "true";
+        2. At line 5, remove the break statement
+        3. Remove the default section
+         */
 
 
 
