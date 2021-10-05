3c078b79cb22cd92d1d826f6bc8c82843c2320e0
mavrenkov <mawro@bk.ru>
2021-10-04 20:43:26 -0500

    swich quiz

---
+++ src/main/java/day_03/LoopQuiz.java
@@ -0,0 +1,66 @@
+package day_03;
+//loop quiz
+public class LoopQuiz {
+
+    public static void main(String[] args) {
+
+
+        //1
+        /*
+        int a= 0;
+        while (a<=6){
+            a+=2;
+        }
+
+         */
+        /*
+        What's the value of a?
+        1. 6
+        2. 8
+        3. 4
+        4. 10
+         */
+        //2
+        /*
+        While loop executes the body of the loop first, before checking the condition
+        1.true
+        2. false
+         */
+        //3
+        /*
+        for(int i=0;i<5;i--){
+            System.out.println("Hello world!");
+        }
+         */
+        /*
+        What will be the output?
+        1. Loop repeats "hello world" 5 times
+        2. Infinite loop
+        3. Loop won't run
+        4. compile error
+         */
+        //4
+        /*
+        int z = 5;
+        for (int i = 5; i > 0; i--) {
+            z+=i;
+        }
+        System.out.println(z);
+        */
+        /*
+        What is the value of z?
+        1. Compile error
+        2. 20
+        3. 15
+        4. 10
+         */
+
+
+
+
+
+    }
+
+
+
+}
