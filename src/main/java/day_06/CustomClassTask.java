63b03303fcf1cefdd031733baa4c85e4937c052f
mavrenkov <mawro@bk.ru>
2021-10-13 15:17:00 -0500

    Custom class task

---
+++ src/main/java/day_06/CustomClassTask.java
@@ -0,0 +1,14 @@
+package day_06;
+/*
+ create a class called ShoppingList
+                create 5 objects of Item and store them into List of Items
+                calculate the total cost of all Items in the list
+                create toString method + constructor
+
+ */
+public class CustomClassTask {
+
+    //e.g.  Item item1 = new Item ("Apple", 3.25)
+    //      Item item2 = new Item ("Orange", 5.75)
+    // sum?
+}
